e53fc155306e42ad61e8b3d43f5890ff1f131ce6
Jgalindom03 <jgalindom2003@gmail.com>
2024-03-08 09:04:14 +0100

    funciones multiplicar, sumar, restar y dividir arregladas

--- src/Cliente.java
+++ src/Cliente.java
@@ -3,10 +3,11 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Scanner;
+
 public class Cliente {
-	private static final String IP = "192.168.1.111"; // Puedes cambiar a localhost
-	private static final int PUERTO = 1200; //Si cambias aquí el puerto, recuerda cambiarlo en el servidor
-	
+    private static final String IP = "localhost"; // Puedes cambiar a localhost
+    private static final int PUERTO = 1100; //Si cambias aquí el puerto, recuerda cambiarlo en el servidor
+
     public static void main(String[] args) throws RemoteException, NotBoundException {
         Registry registry = LocateRegistry.getRegistry(IP, PUERTO);
         Interfaz interfaz = (Interfaz) registry.lookup("Calculadora"); //Buscar en el registro...
@@ -23,35 +24,35 @@ public class Cliente {
                 eleccion = -1;
             }
 
-            if(eleccion != -1){
+            if (eleccion != -1) {
 
-            	System.out.println("Ingresa el número 1: ");
-            	try{
-                	numero1 = Float.parseFloat(sc.nextLine());
-            	}catch(NumberFormatException e){
-            		numero1 = 0;
-            	}
+                System.out.println("Ingresa el número 1: ");
+                try {
+                    numero1 = Float.parseFloat(sc.nextLine());
+                } catch (NumberFormatException e) {
+                    numero1 = 0;
+                }
 
-            	System.out.println("Ingresa el número 2: ");
-            	try{
-                	numero2 = Float.parseFloat(sc.nextLine());
-            	}catch(NumberFormatException e){
-            		numero2 = 0;
-            	}
+                System.out.println("Ingresa el número 2: ");
+                try {
+                    numero2 = Float.parseFloat(sc.nextLine());
+                } catch (NumberFormatException e) {
+                    numero2 = 0;
+                }
                 switch (eleccion) {
-	                case 0:
-	                    resultado = interfaz.sumar(numero1, numero2);
-	                    break;
-	                case 1:
-	                    break;
-	                case 2:
-	                    resultado = interfaz.multiplicar(numero1, numero1);
-	                    break;
-	                case 3:
-	                    resultado = interfaz.dividir(numero1, numero2);
-	                    break;
-	            }
-
+                    case 0:
+                        resultado = interfaz.sumar(numero1, numero2);
+                        break;
+                    case 1:
+                        resultado = interfaz.restar(numero1, numero2);
+                        break;
+                    case 2:
+                        resultado = interfaz.multiplicar(numero1, numero2);
+                        break;
+                    case 3:
+                        resultado = interfaz.dividir(numero1, numero2);
+                        break;
+                }
                 System.out.println("Resultado => " + String.valueOf(resultado));
                 System.out.println("Presiona ENTER para continuar");
                 sc.nextLine();
